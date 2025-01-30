735d8d109422476ab28d9989ac05095c5b725c4d
vangalakarthik018 <vangalakarthik018@gmail.com>
2025-01-30 19:30:05 +0530

    Add files via upload

---
+++ Power.java
@@ -0,0 +1,9 @@
+public class Power {
+    public static void main(String[] args) {
+        double a=2;
+        double b=4;
+        double c=Math.pow(a, b);
+        System.out.println(c);
+
+    }
+}
